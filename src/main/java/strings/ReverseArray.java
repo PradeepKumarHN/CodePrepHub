21963f427014b378a9db2fc401218b542dce4b4a
Pradeep Kumar H N <prdp4747@gmail.com>
2024-04-20 16:52:15 +0530

    pgm2

--- src/main/java/strings/ReverseArray.java
+++ src/main/java/strings/ReverseArray.java
@@ -1,4 +1,21 @@
 package strings;
 
+import java.util.Arrays;
+
 public class ReverseArray {
+    public static void main(String[] args) {
+        int[] a={1,2,3,4,5};
+        System.out.println("reverse of "+Arrays.toString(a)+" is "+Arrays.toString(reversedArray(a)));
+    }
+    public static int[] reversedArray(int[] a){
+        int left=0,right=a.length-1,temp;
+        while(left<right){
+            temp=a[left];
+            a[left]=a[right];
+            a[right]=temp;
+            left++;
+            right--;
+        }
+        return a;
+    }
 }
