0df8e13b07f4719aad515c71be81874df837ae3a
Pradeep Kumar H N <prdp4747@gmail.com>
2024-04-20 17:41:08 +0530

    added reverse array with for loop method in ReverseArray.java program

--- src/main/java/integers/ReverseArray.java
+++ src/main/java/integers/ReverseArray.java
@@ -5,8 +5,19 @@ import java.util.Arrays;
 public class ReverseArray {
     public static void main(String[] args) {
         int[] a={1,2,3,4,5};
+        int[] b=Arrays.copyOf(a,a.length);
         System.out.println("reverse of "+Arrays.toString(a)+" is "+Arrays.toString(reversedArray(a)));
+        System.out.println("reverse of "+Arrays.toString(b)+" is "+Arrays.toString(reversedArrayUsingForLoop(b)));
     }
+
+    private static int[] reversedArrayUsingForLoop(int[] a) {
+        int[] b=new int[a.length];
+        for (int i = a.length-1; i >=0 ; i--) {
+            b[i]=a[i];
+        }
+        return b;
+    }
+
     public static int[] reversedArray(int[] a){
         int left=0,right=a.length-1,temp;
         while(left<right){
@@ -18,4 +29,5 @@ public class ReverseArray {
         }
         return a;
     }
+
 }
